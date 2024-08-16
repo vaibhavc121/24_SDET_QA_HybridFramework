d9d205592ed685308795a77acabc70d627a51ff1
vaibhavc121 <vaibhavc121@gmail.com>
2024-08-17 00:01:33 +0530

    17-8

--- src/test/java/testBase/BaseClass.java
+++ src/test/java/testBase/BaseClass.java
@@ -3,6 +3,7 @@ package testBase;
 import java.io.File;
 import java.io.FileReader;
 import java.io.IOException;
+import java.net.URL;
 import java.text.SimpleDateFormat;
 import java.time.Duration;
 import java.util.Date;
@@ -12,10 +13,13 @@ import org.apache.commons.lang3.RandomStringUtils;
 import org.apache.logging.log4j.LogManager; //log4j
 import org.apache.logging.log4j.Logger; //log4j
 import org.openqa.selenium.OutputType;
+import org.openqa.selenium.Platform;
 import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.remote.RemoteWebDriver;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Parameters;
@@ -27,6 +31,7 @@ public class BaseClass
 	public Logger logger; // log4j
 	public Properties p;
 
+	@SuppressWarnings("deprecation")
 	@BeforeClass(groups =
 	{ "sanity", "regression", "master" })
 	@Parameters(
@@ -40,22 +45,66 @@ public class BaseClass
 
 		logger = LogManager.getLogger(this.getClass()); // log4j2
 
-		switch (br.toLowerCase())
+		// code for execution on selenium grid
+		if (p.getProperty("execution_env").equals("remote"))
 		{
-		case "chrome":
-			driver = new ChromeDriver();
+			DesiredCapabilities capabilities = new DesiredCapabilities();
 
-			break;
+			// OS (we are getting os from xml file)
+			if (os.equalsIgnoreCase("windows"))
+			{
+				capabilities.setPlatform(Platform.WIN11);
+			}
+			else if (os.equalsIgnoreCase("mac"))
+			{
+				capabilities.setPlatform(Platform.MAC);
+			}
+			else
+			{
+				System.out.println("no matching os");
+				return; // It will automatically exit
+			}
 
-		case "edge":
-			driver = new EdgeDriver();
+			// browser (we are getting browser from xml file)
+			switch (br.toLowerCase())
+			{
+			case "chrome":
+				capabilities.setBrowserName("chrome");
 
-			break;
+				break;
 
-		default:
-			System.out.println("invalid browser name");
-			return; // return- totally exit from the execution
+			case "edge":
+				capabilities.setBrowserName("MicrosoftEdge");
 
+				break;
+
+			default:
+				System.out.println("No matching browser ");
+				return; // It will automatically exit from switch case statement
+			}
+
+			driver = new RemoteWebDriver(new URL("http://10.101.223.24:4444/wd/hub"), capabilities);
+
+		}
+
+		if (p.getProperty("execution_env").equals("local"))
+		{
+			switch (br.toLowerCase())
+			{
+			case "chrome":
+				driver = new ChromeDriver();
+
+				break;
+
+			case "edge":
+				driver = new EdgeDriver();
+
+				break;
+
+			default:
+				System.out.println("invalid browser name");
+				return; // return- totally exit from the execution
+			}
 		}
 
 		driver.manage().deleteAllCookies();
