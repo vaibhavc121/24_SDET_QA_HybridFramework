5d7a07b856d02ab66f9803e06de4acac599ed769
vaibhavc121 <vaibhavc121@gmail.com>
2024-08-27 00:04:24 +0530

    27-8

---
+++ run.bat
@@ -0,0 +1,6 @@
+D:
+cd #Testing
+cd Automation Testing
+cd SDET_QA
+cd 24_SDET_QA_HybridFramework
+mvn test
---
+++ src/test/java/testCases/TC_AddressBookTest.java
@@ -0,0 +1,14 @@
+package testCases;
+
+import org.testng.annotations.Test;
+
+import testBase.BaseClass;
+
+public class TC_AddressBookTest extends BaseClass
+{
+	@Test
+	public void verify_addBook()
+	{
+
+	}
+}
